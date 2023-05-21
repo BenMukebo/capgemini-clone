712204baa8d78193a061e4113c2851a9cb1d2d4e
BenMukebo <l.benkasmukebo7@gmail.com>
2023-05-21 17:28:19 +0200

    Add some tailwind config default value

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,31 @@ module.exports = {
   ],
 
   theme: {
-    extend: {},
+    screens: {
+      xm: "375px",
+      sm: "480px",
+      md: "786px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1480px",
+    },
+    extend: {
+      colors: {
+        'primary': '#171717',
+        'secondary': '#0070ad',
+        'white': '#fff',
+        'black': '#15011d',
+      },
+      backgroundImage: {
+        'intro-img': "url('./src/img/intro-bg.jpeg')",
+        // 'footer-texture': "url('/img/footer-texture.png')",
+      },
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif'],
+        'serif': ['Poppins', 'serif'],
+        // 'body': ['Open Sans', '...fontFamilly.sans'],
+      },
+    }
   },
   plugins: [],
 }
-
